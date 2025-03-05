74216e2cd1b4aae2942f2a9a4ef736fae14091d5
Brenden Bishop <btbishop93@gmail.com>
2025-03-05 17:38:53 -0500

    Fixing android build issues

--- android/app/src/main/kotlin/com/example/jumblebook/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/jumblebook/MainActivity.kt
@@ -1,11 +1,6 @@
-package com.example.jumblebook;
+package xyz.bsquared.jumblebook
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+class MainActivity: FlutterActivity() {
 }
